f061e27742b094d81cb44a95f78804d7169a1415
ruskaof <199-41@mail.ru>
2025-02-15 17:21:18 +0300

    make storage in volume

--- consensus/kv-store-ratis/src/main/java/ru/itmo/rusinov/consensus/kv/store/ratis/KvStoreApplication.java
+++ consensus/kv-store-ratis/src/main/java/ru/itmo/rusinov/consensus/kv/store/ratis/KvStoreApplication.java
@@ -1,42 +1,61 @@
 package ru.itmo.rusinov.consensus.kv.store.ratis;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.grpc.GrpcConfigKeys;
+import org.apache.ratis.metrics.impl.JvmMetrics;
 import org.apache.ratis.protocol.RaftGroup;
 import org.apache.ratis.protocol.RaftGroupId;
 import org.apache.ratis.protocol.RaftPeer;
 import org.apache.ratis.protocol.RaftPeerId;
 import org.apache.ratis.server.RaftServer;
 import org.apache.ratis.server.RaftServerConfigKeys;
+import org.apache.ratis.server.storage.RaftStorage;
 import org.apache.ratis.statemachine.StateMachine;
 import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 import org.apache.ratis.util.LifeCycle;
 import ru.itmo.rusinov.consensus.kv.store.ratis.server.KvStateMachine;
 import ru.itmo.rusinov.consensus.kv.store.ratis.server.MapDbKvDatabase;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class KvStoreApplication {
-    private static void run(String id, int port, List<RaftPeer> peers, String groupId) throws IOException, InterruptedException {
+
+    @SneakyThrows
+    private static File createStorageDir(String path) {
+        if (Objects.isNull(path)) {
+            return Files.createTempDirectory("kvStorage").toFile();
+        } else {
+            var file = new File(path);
+            file.mkdirs();
+            return file;
+        }
+    }
+
+    @SneakyThrows
+    private static void run(String id, int port, List<RaftPeer> peers, String groupId, String storagePath) {
         RaftPeerId peerId = RaftPeerId.valueOf(id);
         RaftProperties properties = new RaftProperties();
 
         GrpcConfigKeys.Server.setPort(properties, port);
 
-        RaftServerConfigKeys.setStorageDir(properties, Collections.singletonList(Files.createTempDirectory("kvStorage").toFile()));
+        RaftServerConfigKeys.setStorageDir(properties, Collections.singletonList(createStorageDir(storagePath)));
         StateMachine stateMachine = new KvStateMachine(new MapDbKvDatabase());
 
         final RaftGroup raftGroup = RaftGroup.valueOf(RaftGroupId.valueOf(ByteString.copyFromUtf8(groupId)), peers);
         RaftServer raftServer = RaftServer.newBuilder()
                 .setServerId(peerId)
                 .setStateMachine(stateMachine)
+                .setOption(RaftStorage.StartupOption.RECOVER)
                 .setProperties(properties).setGroup(raftGroup)
                 .build();
         raftServer.start();
@@ -47,8 +66,6 @@ public class KvStoreApplication {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        log.info("Starting raft kv store peer with peerId={}, port={}, peers={}", System.getenv("RATIS_KV_PEER_ID"), System.getenv("RATIS_KV_PORT"), System.getenv("RATIS_KV_PEERS"));
-
         String peerId = System.getenv("RATIS_KV_PEER_ID");
         int port = Integer.parseInt(System.getenv("RATIS_KV_PORT"));
         var peers = Arrays.stream(System.getenv("RATIS_KV_PEERS").split(","))
@@ -61,7 +78,8 @@ public class KvStoreApplication {
                 })
                 .toList();
         String groupId = System.getenv("RATIS_KV_GROUP_ID");
+        String storagePath = System.getenv("RATIS_KV_STORAGE_PATH");
 
-        run(peerId, port, peers, groupId);
+        run(peerId, port, peers, groupId, storagePath);
     }
 }
